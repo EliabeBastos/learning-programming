df510777c4f2efb267e5dfd6d5fccdba04764dd2
eliabe.bastos.dias <eliabebastosdias123@gmail.com>
2023-02-22 16:31:31 -0300

    second version of project one of rocketseat

--- rocketseat/node/project-one/version.2.0.0/src/index.js
+++ rocketseat/node/project-one/version.2.0.0/src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const freePlan = (user.pro === false) && user.todos.length < 10;
+  const proPlan = user.pro;
+
+  if (!(freePlan || proPlan)) {
+    return response.status(403).json({ error: 'Not allowed to create todo' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { headers: { username }, params: { id }} = request;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id not validate' });
+  }
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { params: { id }} = request;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
